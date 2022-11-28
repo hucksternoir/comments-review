4167f564a1d9bd5d3f78f78804b2627e6c075f40
hucksternoir <itaansantos171199@gmail.com>
2022-11-28 10:54:09 -0300

    style changes and previous button

--- script.js
+++ script.js
@@ -2,38 +2,66 @@ const pessoasComentario = [
   {
     foto: "imgs/imagem-1.jpg",
     empresa: "Facebook",
-    comentario: "Gostei muito",
+    comentario:
+      "I love it!! This service helps me a lot in my work. These tools  are very easy to use.",
   },
   {
     foto: "imgs/imagem-2.jpg",
     empresa: "Google",
-    comentario: "Gostei bastante",
+    comentario:
+      "It's really very useful! These tools reduce my work time a lot, in an easy and fast way.",
   },
   {
     foto: "imgs/imagem-3.jpg",
     empresa: "Twitter",
-    comentario: "Gostei pakas",
+    comentario:
+      "Congratulations to the developers of this service! Always delivering my projects quickly.",
   },
 ];
-let indexFoto = 1;
-let indexEmpresa = 1;
-let indexComent = 1;
-function proximo(event) {
-  console.log([indexFoto, indexComent, indexEmpresa]);
+
+let indexFotoProximo = 1;
+let indexEmpresaProximo = 1;
+let indexComentProximo = 1;
+
+function proximo() {
+  console.log([indexFotoProximo, indexComentProximo, indexEmpresaProximo]);
   document
     .querySelector(".imagem-review img")
-    .setAttribute("src", pessoasComentario[indexFoto++].foto);
+    .setAttribute("src", pessoasComentario[indexFotoProximo++].foto);
   document.querySelector(".empresa-nome").innerHTML =
-    pessoasComentario[indexEmpresa++].empresa;
+    pessoasComentario[indexEmpresaProximo++].empresa;
   document.querySelector(".comentario").innerHTML =
-    pessoasComentario[indexComent++].comentario;
-  if (indexFoto > 2) {
-    indexFoto = 0;
-    indexEmpresa = 0;
-    indexComent = 0;
+    pessoasComentario[indexComentProximo++].comentario;
+  if (indexFotoProximo > 2) {
+    indexFotoProximo = 0;
+    indexEmpresaProximo = 0;
+    indexComentProximo = 0;
   }
 }
 
-const botao = document.querySelector("#proximo");
+let indexFotoAnterior = 0;
+let indexEmpresaAnterior = 0;
+let indexComentAnterior = 0;
 
-botao.addEventListener("click", proximo);
+function anterior() {
+  console.log([indexFotoAnterior, indexComentAnterior, indexEmpresaAnterior]);
+  document
+    .querySelector(".imagem-review img")
+    .setAttribute("src", pessoasComentario[indexFotoAnterior--].foto);
+  document.querySelector(".empresa-nome").innerHTML =
+    pessoasComentario[indexEmpresaAnterior--].empresa;
+  document.querySelector(".comentario").innerHTML =
+    pessoasComentario[indexComentAnterior--].comentario;
+
+  if (indexFotoAnterior < 0) {
+    indexFotoAnterior = 2;
+    indexEmpresaAnterior = 2;
+    indexComentAnterior = 2;
+  }
+}
+
+const botaoProximo = document.querySelector("#proximo");
+const botaoAnterior = document.querySelector("#anterior");
+
+botaoProximo.addEventListener("click", proximo);
+botaoAnterior.addEventListener("click", anterior);
